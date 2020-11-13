589f4fba326150f6b451dce0c8143a776c1c9fec
Jooonyub <59817144+Jooonyub@users.noreply.github.com>
2020-11-13 15:42:24 +0900

    1113 afternoon_roster.py
    SQL로부터 query를 사용하여 데이터 불러오기
    SELECT DISTINCT house FROM students

--- pset7/houses/roster.py
+++ pset7/houses/roster.py
@@ -2,3 +2,27 @@
 import sqlite3
 from sys import argv, exit
 
+if len(argv) != 2 :
+    print("error")
+    exit()
+
+conn = sqlite3.connect("/Users/joonyubkim/workspace/python/pset7/houses/students.db")
+db = conn.cursor()
+
+find_house = argv[1]
+
+house = db.execute("""
+SELECT DISTINCT house FROM students
+""")
+
+'''
+for word in house :
+    print(word)
+'''
+
+conn.commit()
+
+
+
+
+
